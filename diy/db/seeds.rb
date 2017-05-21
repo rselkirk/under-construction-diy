7bc928442fd2ea4f62d2f361e00deda8674efdb7
Rachel Selkirk <rachel.selkirk@gmail.com>
2017-05-21 22:15:21 +0000

    changed database seeds to be able to test more data

--- diy/db/seeds.rb
+++ diy/db/seeds.rb
@@ -12,43 +12,44 @@ def open_asset(file_name)
   File.open(Rails.root.join('db', 'seed_assets', file_name))
 end
 
-# USERS
 
-puts "Re-creating Users ..."
-
-User.create([{
-  first_name: 'fayez',
-  last_name: 'o',
-  email:'f@gmail.com',
-  password_digest: '1234'
- }])
 
 # PROJECTS
 
 puts "Re-creating Projects ..."
 
 Project.create!([{
-  title: 'Create a picnic table',
-  summary: Faker::Hipster.paragraph(3),
+  title: 'Rustic Park Bench',
+  summary: Faker::Hipster.paragraph(1),
   instructions: Faker::Hipster.paragraph(4),
   origin: true,
   user_id: 1
  }])
 
 Project.create!([{
-  title: 'Create a picnic table',
-  summary: Faker::Hipster.paragraph(3),
-  instructions: Faker::Hipster.paragraph(4),
+  title: 'Family Picnic Table',
+  summary: Faker::Hipster.paragraph(1),
+  instructions: Faker::Hipster.paragraph(3),
   origin: true,
-  user_id: 1
+  user_id: 2
  }])
+
+Project.create!([{
+  title: 'Industrial Bookshelf',
+  summary: Faker::Hipster.paragraph(1),
+  instructions: Faker::Hipster.paragraph(5),
+  origin: true,
+  user_id: 3
+ }])
+
 
 # TAGS
 
 puts "Re-creating Tags ..."
 
 tag1 = Tag.create!([{ name: 'woodworking'}])
-tag2 = Tag.create!([{ name: 'coding'}])
+tag2 = Tag.create!([{ name: 'outdoor'}])
+tag2 = Tag.create!([{ name: 'furniture'}])
 
 # ProjectTAGS
 
@@ -56,12 +57,32 @@ puts "Re-creating ProjectTags ..."
 
 ProjectTag.create([{
    project_id: 1,
-   tag_id:1
+   tag_id:3
  }])
 
 ProjectTag.create([{
   project_id: 1,
+  tag_id:1
+}])
+
+ProjectTag.create([{
+  project_id: 1,
   tag_id:2
+}])
+
+ProjectTag.create([{
+  project_id: 2,
+  tag_id:2
+}])
+
+ProjectTag.create([{
+  project_id: 2,
+  tag_id:1
+}])
+
+ProjectTag.create([{
+  project_id: 3,
+  tag_id:3
 }])
 
 # USERSPROJECTS
@@ -73,6 +94,41 @@ UserProject.create!([{
   project_id:1,
   completes: true,
   saves: false
+ }])
+
+ UserProject.create!([{
+  user_id: 1,
+  project_id:2,
+  completes: true,
+  saves: true
+ }])
+
+ UserProject.create!([{
+  user_id: 1,
+  project_id:3,
+  completes: true,
+  saves: true
+ }])
+
+ UserProject.create!([{
+  user_id: 2,
+  project_id:3,
+  completes: true,
+  saves: true
+ }])
+
+ UserProject.create!([{
+  user_id: 2,
+  project_id:2,
+  completes: true,
+  saves: false
+ }])
+
+  UserProject.create!([{
+  user_id: 3,
+  project_id:1,
+  completes: false,
+  saves: true
  }])
 
 
@@ -89,7 +145,30 @@ puts "Re-creating Rating..."
 # RATING
 
 Rating.create([{
+  rating: 2
+}])
+
+Rating.create([{
+  rating: 3
+}])
+
+Rating.create([{
+  rating: 1
+}])
+
+Rating.create([{
   rating: 4
+}])
+Rating.create([{
+  rating: 1
+}])
+
+Rating.create([{
+  rating: 3
+}])
+
+Rating.create([{
+  rating: 2
 }])
 
 puts "Re-creating Reviews..."
@@ -101,6 +180,60 @@ Review.create([{
   project_id: 1,
   user_id: 1,
   rating_id:1
+}])
+
+Review.create([{
+  content: Faker::Hipster.paragraph(2),
+  time: 5,
+  cost: 3,
+  project_id: 1,
+  user_id: 2,
+  rating_id:2
+}])
+
+Review.create([{
+  content: Faker::Hipster.paragraph(3),
+  time: 1,
+  cost: 3,
+  project_id: 2,
+  user_id: 1,
+  rating_id:3
+}])
+
+Review.create([{
+  content: Faker::Hipster.paragraph(2),
+  time: 4,
+  cost: 3,
+  project_id: 2,
+  user_id: 2,
+  rating_id:4
+}])
+
+Review.create([{
+  content: Faker::Hipster.paragraph(3),
+  time: 7,
+  cost: 3,
+  project_id: 3,
+  user_id: 1,
+  rating_id:5
+}])
+
+Review.create([{
+  content: Faker::Hipster.paragraph(2),
+  time: 4,
+  cost: 3,
+  project_id: 3,
+  user_id: 2,
+  rating_id:6
+}])
+
+Review.create([{
+  content: Faker::Hipster.paragraph(1),
+  time: 3,
+  cost: 3,
+  project_id: 3,
+  user_id: 3,
+  rating_id:7
 }])
 
 puts "Re-creating ProjectUploads..."
@@ -118,7 +251,7 @@ puts "Re-creating ReviewUploads..."
 
 ReviewUpload.create([{
   image_url: open_asset('diy.jpg'), 
-  review_id: 1
+  review_id: 2
 }])
 
 
