06ea5531a3842465fcb8025a2b34d1463d896988
fayezosaadi <fayezosaadi@gmail.com>
2017-05-21 01:17:07 +0000

    feature-signup-login

--- diy/config/routes.rb
+++ diy/config/routes.rb
@@ -7,8 +7,14 @@ Rails.application.routes.draw do
     resources :reviews, only: [:create]
   end
 
-  resources :sessions, only: [:new, :create, :destroy]
-  resources :users, except: [:index]
+  resources :users, only: [:create, :show]
+  get '/signup' => 'users#new'
+  
+  resources :sessions
+  get '/login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  delete '/logout' => 'sessions#destroy'
+
 
   resources :reviews, only: [:destroy]
 end
