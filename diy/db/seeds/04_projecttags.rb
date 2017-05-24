71bac94fa98d5a4ba52b13e3b06e95a9cfffe3f7
Rachel Selkirk <rachel.selkirk@gmail.com>
2017-05-24 22:02:52 +0000

    minor linting

--- diy/db/seeds/04_projecttags.rb
+++ diy/db/seeds/04_projecttags.rb
@@ -15,24 +15,24 @@ ProjectTag.create!(
 ProjectTag.create(
   project_id: 1,
   tag_id:1
-}])
+)
 
 ProjectTag.create(
   project_id: 1,
   tag_id:2
-}])
+)
 
 ProjectTag.create(
   project_id: 2,
   tag_id:2
-}])
+)
 
 ProjectTag.create(
   project_id: 2,
   tag_id:1
-}])
+)
 
 ProjectTag.create(
   project_id: 3,
   tag_id:3
-}])
+)
--- diy/db/seeds/06_comments.rb
+++ diy/db/seeds/06_comments.rb
@@ -1,8 +1,8 @@
 puts "Re-creating Comments..."
 
 
-Comment.create!([{
+Comment.create!(
   content: Faker::Hipster.paragraph(3),
   user_id: 1,
   project_id: 2
-}])
+)
