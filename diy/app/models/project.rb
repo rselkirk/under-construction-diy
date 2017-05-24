436bccf3577933f3caeeaf5f019cb22876b4fb6b
Amber Tsosie <atxambra@gmail.com>
2017-05-24 18:19:40 +0000

    Created instance method 'avg_rating' for class Project. To redefine this method, find it in project.rb (Project model).

--- diy/app/models/project.rb
+++ diy/app/models/project.rb
@@ -1,5 +1,7 @@
 class Project < ApplicationRecord
 
+  attr_accessor :avg_rating
+
   belongs_to :user
   has_many :reviews
   has_many :comments
@@ -7,5 +9,9 @@ class Project < ApplicationRecord
   has_many :project_statuses
 
   accepts_nested_attributes_for :project_uploads, allow_destroy: true
+
+  def avg_rating
+    self.reviews.average(:rating).to_i
+  end
 
 end
