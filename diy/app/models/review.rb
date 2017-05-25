a0d71752650f5da14a0279e0e57b5aee289a32ba
Rachel Selkirk <rachel.selkirk@gmail.com>
2017-05-25 21:54:31 +0000

    added columns for avg cost, avg time, calcs for counts

--- diy/app/models/review.rb
+++ diy/app/models/review.rb
@@ -4,7 +4,7 @@ class Review < ApplicationRecord
   belongs_to :user
   has_many :review_uploads
   
-  after_create :update_project_average_rating
+  after_create :update_project_average_rating, :update_project_review_count, :update_project_complete_count, :update_project_save_count, :update_project_average_cost, :update_project_average_time
   
   validates :content, presence: true
   validates :project_id, presence: true
@@ -15,5 +15,26 @@ class Review < ApplicationRecord
   def update_project_average_rating
     self.project.update_average_rating
   end
+
+  def update_project_review_count
+    self.project.update_review_count
+  end
+
+  def update_project_complete_count
+    self.project.update_complete_count
+  end
+
+  def update_project_save_count
+    self.project.update_save_count
+  end
+
+  def update_project_average_cost
+    self.project.update_average_cost
+  end
+
+  def update_project_average_time
+    self.project.update_average_time
+  end
+
   
 end
