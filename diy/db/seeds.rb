d539c6db7477df5f6a78d73f41ae48640c6fb44d
SomeChineseGuy <alvin.cl.ng@gmail.com>
2017-05-26 18:15:29 -0700

    deployment

--- diy/db/seeds.rb
+++ diy/db/seeds.rb
@@ -1,17 +1,17 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 
-puts "Seeding Data ..."
+puts "Ensuring ES indexes exist..."
+puts "Destorying all then Seeding Data ..."
+
+[Comment, ProjectTagm ProjectUpload, ProjectStatus, Review, Project, ReviewUpload, User].each do |model_class|
+  model_class.destroy_all
+  model_class.__elasticsearch__.create_index! force: true
+end
 
 
-Comment.destroy_all
-ProjectTag.destroy_all
-ProjectUpload.destroy_all
-ProjectStatus.destroy_all
-Review.destroy_all
-Project.destroy_all
-ReviewUpload.destroy_all
-User.destroy_all
+
+
 
 # load seeds from respective seed files
 Dir[File.join(Rails.root, 'db', 'seeds', '*.rb')].sort.each { |seed| load seed }
