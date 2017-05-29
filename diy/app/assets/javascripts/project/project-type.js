fac677c97bc21616057bef46b3eab83820814162
Amber Tsosie <atxambra@gmail.com>
2017-05-29 01:54:53 +0000

    Added to global layout stylesheet and modified some html to fit new stylesheet.

--- diy/app/assets/javascripts/project/project-type.js
+++ diy/app/assets/javascripts/project/project-type.js
@@ -1,15 +1,17 @@
 $(document).ready(function onReady() {
+
   $( "#create_project" ).click(function() {
-    $( "#external_link_toggle" ).hide()
+    $( "#external_link_toggle" ).hide();
     $( "#create_project_toggle" ).fadeIn( 400, function() {
       $("#project_title ").focus();
     });
   });
 
   $( "#external_link" ).click(function() {
-    $( "#create_project_toggle" ).hide()
+    $( "#create_project_toggle" ).hide();
     $( "#external_link_toggle" ).fadeIn( 400, function() {
       $("#project_title").focus();
     });
   });
+
 });
