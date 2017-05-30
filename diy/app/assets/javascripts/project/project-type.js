008d7eb97fa2d890bfbbf3b15b5afa194ff01d67
Fayez Saadi <fayezosaadi@gmail.com>
2017-05-30 15:22:17 -0700

    forms-styling

--- diy/app/assets/javascripts/project/project-type.js
+++ diy/app/assets/javascripts/project/project-type.js
@@ -3,44 +3,27 @@ $(document).ready(function onReady() {
   $("#project_url").focus();
   $('#create_project').on('click', function () {
     $('.card').toggleClass('flipped');
-    $("#project_title").focus();
+    $('#project_title').focus();
   });
 
-  $('.fa-chevron-right').on('click', function () {
+  $('.fa-arrow-right').on('click', function () {
     $('.card').toggleClass('flipped');
   });
 
-
-  // $( "#create_project" ).click(function() {
-  //   $( "#external_link_toggle" ).hide();
-  //   $( "#create_project_toggle" ).fadeIn( 400, function() {
-  //     $("#project_title ").focus();
-  //   });
-  // });
-
-  // $( "#external_link" ).click(function() {
-  //   $( "#create_project_toggle" ).hide();
-  //   $( "#external_link_toggle" ).fadeIn( 400, function() {
-  //     $("#project_url").focus();
-  //   });
-  // });
+ $('.fa-home').on('click', function () {
+    window.location.href='/';
+  });
 
   $('.new_project').on('ajax:success', (e, data, status, xhr) => {
-    console.log('test')
+    console.log('test');
     $(".link").remove();
     $(".external_link").show();
     $('#url-show').find("input[type=text], textarea").val(data.url)
-
-  //   $("#external_link_toggle").remove();
-    // $('#url-show').find("input[type=text], textarea").val(data.url)
     $('#title-show').find("input[type=text], textarea").val(data.title)
     $('#description-show').find("input[type=text], textarea").val(data.description)
     $('#image-show-upload').find("input[type=text], textarea").val(data.image)
     $('#image-show').append(`<img src="${data.image}">`);
-
   }).on('ajax:error', (e, xhr, status, error) => {
-  //   console.log('error')
-  //   $('#url-show').append('<p>ERROR</p>')
   });
 });
 
