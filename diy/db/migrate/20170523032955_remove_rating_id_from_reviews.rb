c34f9eb541397ee015d8f0e334e7e287e6bcd655
SomeChineseGuy <alvin.cl.ng@gmail.com>
2017-05-29 20:30:36 -0700

    Removed a non connected DB endingh with 2955

--- diy/db/migrate/20170523032955_remove_rating_id_from_reviews.rb
+++ diy/db/migrate/20170523032955_remove_rating_id_from_reviews.rb
@@ -1,5 +1,5 @@
 class RemoveRatingIdFromReviews < ActiveRecord::Migration[5.0]
   def change
-    remove_reference :reviews, :rating, foreign_key: true
+    # remove_reference :reviews, :rating, foreign_key: true
   end
 end
