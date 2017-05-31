6351290c4fdd476155bd6b05ac753ad1fcf7361e
SomeChineseGuy <alvin.cl.ng@gmail.com>
2017-05-30 17:22:33 -0700

    Resolve conflict with master

--- diy/db/seeds.rb
+++ diy/db/seeds.rb
@@ -11,10 +11,6 @@ puts "Destroying all then Seeding Data ..."
   model_class.destroy_all
 end
 
-<<<<<<< HEAD
-
-=======
->>>>>>> master
 # load seeds from respective seed files
 Dir[File.join(Rails.root, 'db', 'seeds', '*.rb')].sort.each { |seed| load seed }
 
