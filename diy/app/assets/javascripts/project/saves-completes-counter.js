d81ff88debf58c78f38d83ed7627c99c2070743b
Fayez Saadi <fayezosaadi@gmail.com>
2017-05-31 09:59:37 -0700

    complete/save buttons

--- diy/app/assets/javascripts/project/saves-completes-counter.js
+++ diy/app/assets/javascripts/project/saves-completes-counter.js
@@ -1,13 +1,28 @@
 $(document).ready(function onReady() {
 
+  $(".mark_complete").click(function(){
+    // $(".mark_complete").hide();
+    // $(".completed").show();
+    $(this).toggleClass("completed");
+    // $( "div.mark_complete" ).replaceWith( "completed" );
+  });
+
+  $(".completed").click(function(){
+    $(this).toggleClass("mark_complete");
+  });
+
+
   $('#saves-count').on('ajax:success', (e, data, status, xhr) => {
-    $('#saves-count-show').text(data.saves);
+    $('.fa-bookmark-o').text(`(${data.saves})`);
+    console.log(data.saves)
   }).on('ajax:error', (e, xhr, status, error) => {
     $('#saves-count').append('<p>ERROR</p>');
   });
 
   $('#completes-count').on('ajax:success', (e, data, status, xhr) => {
-    $('#completes-count-show').text(data.completes);
+    console.log(data.completes)
+    // console.log(data.status)
+    $('.fa-wrench').text(`(${data.completes})`);
   }).on('ajax:error', (e, xhr, status, error) => {
     $('#completes-count').append('<p>ERROR</p>');
   });
