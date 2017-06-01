43a00f5a71909cacb207d4e08555642cc3e1be4b
Fayez Saadi <fayezosaadi@gmail.com>
2017-05-31 19:26:59 -0700

    project-modal-refactor

--- diy/app/assets/javascripts/project/modal.js
+++ diy/app/assets/javascripts/project/modal.js
@@ -1,28 +1,44 @@
 $(document).ready(function(){
-  var title, content, fetchAndInsert;
+  var title, content, fetchAndInsert, toggleModal;
+  var overlayLayer = $(".overlay-layer");
+  var backgroundLayer = $(".background-layer");
   title = $('.title');
   modal = $('.modal');
 
 
+  toggleModal = function() {
+    backgroundLayer.addClass('blur');
+    modal.addClass('open');
+    overlayLayer.addClass('open');
+  }
+
   fetchAndInsert = function(href){
-    // console.log(href);
     $.ajax({
       url: 'http://localhost:8080/' + href,
       cache: false,
       success: function(data){
+        toggleModal();
         modal.html(data);
-        modal.addClass('open');
-
       }
     });
   };
-  $(window).on('popstate', function(){
-    fetchAndInsert("/");
-  });
+
+  // $(window).on('popstate', function(){
+  //   fetchAndInsert("/");
+  // });
+
   title.find('a').on('click', function(e){
     var href = $(this).attr('href');
     history.pushState(null, null, href);
     fetchAndInsert(href);
     e.preventDefault();
   });
+
+ overlayLayer.on('click', function(){
+    overlayLayer.removeClass('open');
+    modal.removeClass('open');
+    backgroundLayer.removeClass('blur');
+  });
+
+
 });
