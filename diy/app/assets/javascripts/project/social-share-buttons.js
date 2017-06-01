5109808b8490e827dc4328db4a05bebe210b1353
Amber Tsosie <atxambra@gmail.com>
2017-06-01 02:04:07 +0000

    Merged master (after PR#147) into local branch 'copy-amber/128', resolved conflicts from master, and squashing bugs caused by Rails running scripts on all pages. During debugging process, I refactored code to pull only first project image (no more looping through array of images for projects).

--- diy/app/assets/javascripts/project/social-share-buttons.js
+++ diy/app/assets/javascripts/project/social-share-buttons.js
@@ -1,29 +1,37 @@
-$(document).ready(function onReady() {
+//************************************
 
-  var pinterestShare = document.querySelector('[data-js="pinterest-share"]');
-  var twitterShare = document.querySelector('[data-js="twitter-share"]');
-  var facebookShare = document.querySelector('[data-js="facebook-share"]');
+// This is commented out because it conflicts with other scripts running (profile tabs).
+// We need to figure out how to make scripts run only on their designated pages.
+// If necessary, we can manually import them into each page, but that's no ideal.
 
-  pinterestShare.onclick = function(event) {
-    event.preventDefault();
-    var pinterestWindow = window.open('http://pinterest.com/pin/create/button/?url=' + document.URL, 'pinterest-popup', 'height=350,width=600');
-    if(pinterestWindow.focus) { pinterestWindow.focus(); }
-    return false;
-  };
+//************************************
 
-  twitterShare.onclick = function(event) {
-    event.preventDefault();
-    var twitterWindow = window.open('https://twitter.com/share?url=' + document.URL, 'twitter-popup', 'height=350,width=600');
-    if(twitterWindow.focus) { twitterWindow.focus(); }
-    return false;
-  };
+// $(document).ready(function onReady() {
 
-  facebookShare.onclick = function(event) {
-    event.preventDefault();
-    var facebookWindow = window.open('https://www.facebook.com/sharer/sharer.php?u=' + document.URL, 'facebook-popup', 'height=350,width=600');
-    if(facebookWindow.focus) { facebookWindow.focus(); }
-    return false;
-  };
+//   var pinterestShare = document.querySelector('[data-js="pinterest-share"]');
+//   var twitterShare = document.querySelector('[data-js="twitter-share"]');
+//   var facebookShare = document.querySelector('[data-js="facebook-share"]');
 
-});
+//   pinterestShare.onclick = function(event) {
+//     event.preventDefault();
+//     var pinterestWindow = window.open('http://pinterest.com/pin/create/button/?url=' + document.URL, 'pinterest-popup', 'height=350,width=600');
+//     if(pinterestWindow.focus) { pinterestWindow.focus(); }
+//     return false;
+//   };
+
+//   twitterShare.onclick = function(event) {
+//     event.preventDefault();
+//     var twitterWindow = window.open('https://twitter.com/share?url=' + document.URL, 'twitter-popup', 'height=350,width=600');
+//     if(twitterWindow.focus) { twitterWindow.focus(); }
+//     return false;
+//   };
+
+//   facebookShare.onclick = function(event) {
+//     event.preventDefault();
+//     var facebookWindow = window.open('https://www.facebook.com/sharer/sharer.php?u=' + document.URL, 'facebook-popup', 'height=350,width=600');
+//     if(facebookWindow.focus) { facebookWindow.focus(); }
+//     return false;
+//   };
+
+// });
 
